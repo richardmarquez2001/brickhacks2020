d9dfaab152a27f87c880a864790fe71f959fde28
Aloin <onealainn@gmail.com>
2020-02-08 15:13:10 -0500

    Added map marker placing function

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
-function initMap() {
-  // The location of Uluru
-  var uluru = {lat: -25.344, lng: 131.036};
-  // The map, centered at Uluru
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
+// CREATE APP PROPERTIES
+app = {
+  map: undefined,
+  markers: undefined
 }
- 
+
+
+// CREATES THE MAP FUNCTION
+function initMap() {
+  // FIRST LOCATION
+  var start = {lat: -25.344, lng: 131.036};
+  // The map, centered at Uluru
+  app.map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: start}
+    );
+
+  createCrime(start);
+  console.log(markers)
+}
+
+
+// CREATE CRIME MARKER FUNCTION,
+function createCrime(position){
+  // CREATE NEW CRIME MARKER
+  var marker = new google.maps.Marker({
+    position: position,
+    map: app.map,
+    icon: {
+        path: google.maps.SymbolPath.CIRCLE,
+        fillColor: '#d45542',
+        fillOpacity: 0.6,
+        strokeColor: '#7d3429',
+        strokeOpacity: 0.9,
+        strokeWeight: 1,
+        scale: 7
+    }});
+  app.push(marker);
+}
