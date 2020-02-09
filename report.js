ec4e06f54431fd0b1acc5e21774e4bc18ac18a1a
dylanle18 <45722863+dylanle18@users.noreply.github.com>
2020-02-08 23:44:12 -0500

    olololol

--- report.js
+++ report.js
@@ -1,6 +1,11 @@
 const report_button = document.getElementById("post-button");
 const report_form = document.getElementById("input-form");
+const close_button = document.getElementById("close-button");
 
 report_button.onclick = () => {
     report_form.classList.remove("hide");
+};
+
+close_button.onclick = () => {
+    report_form.classList.add("hide");
 };
