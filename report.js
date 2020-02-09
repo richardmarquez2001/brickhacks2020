4283821685665964e5cd8f94ef9b42fa317ca748
oceansam <s2chowdhury@ryerson.ca>
2020-02-09 02:43:57 -0500

    Fixed small stuff

--- report.js
+++ report.js
@@ -6,19 +6,26 @@ const report_menu = document.getElementById("report-menu");
 const login_menu = document.getElementById("login-menu");
 const register_menu = document.getElementById("register-menu");
 
-//report_menu.style.display = "none";
+report_menu.style.display = "none";
 login_menu.style.display = "none";
 register_menu.style.display = "none";
 
 app = {
   csrf: '4#yr..~%hN%o17oJ-jg%"$-)W#V1%Z2XLZzd6z8maI^mA]*]2t.FyklY8(!KM',
-  token: undefined,
+  token: undefined
+};
+
+function openMenu(menu) {
+  menu.style.display = "block";
+}
+function closeMenu(menu) {
+  menu.style.display = "none";
 }
 
-function openMenu(menu){menu.style.display = "block"};
-function closeMenu(menu){menu.style.display = "none"};
-
-function tryReport(){
-  if(app.token == undefined){openMenu(login_menu)}
-  else                      {openMenu(report_menu)}
+function tryReport() {
+  if (app.token == undefined) {
+    openMenu(login_menu);
+  } else {
+    openMenu(report_menu);
+  }
 }
