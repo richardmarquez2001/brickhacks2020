136161ed305ffa8f60bfd5335c49cdc8d517402d
Aloin <onealainn@gmail.com>
2020-02-09 01:51:24 -0500

    updated

--- report.js
+++ report.js
@@ -9,10 +9,16 @@ const register_menu = document.getElementById("register-menu");
 report_menu.style.display = "none";
 //login_menu.style.display = "none";
 register_menu.style.display = "none";
-function openMenu(menu){
-  menu.style.display = "block"
-};
 
-function closeMenu(menu){
-  menu.style.display = "none"
-};
+app = {
+  csrf: '4#yr..~%hN%o17oJ-jg%"$-)W#V1%Z2XLZzd6z8maI^mA]*]2t.FyklY8(!KM',
+  token: undefined,
+}
+
+function openMenu(menu){menu.style.display = "block"};
+function closeMenu(menu){menu.style.display = "none"};
+
+function tryReport(){
+  if(app.token == undefined){openMenu(login_menu)}
+  else                      {openMenu(report_menu)}
+}
