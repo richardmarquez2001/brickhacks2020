12574370e8c80670e53338c0ec962fc97a7c48ab
Aloin <onealainn@gmail.com>
2020-02-09 01:36:15 -0500

    Adding Menus

--- report.js
+++ report.js
@@ -2,13 +2,17 @@ const report_button = document.getElementById("post-button");
 const report_form = document.getElementById("input-form");
 const close_button = document.getElementById("close-button");
 
-const report_menu = document.getElementById("report-menu")
-report_menu.style.display = "none";
+const report_menu = document.getElementById("report-menu");
+const login_menu = document.getElementById("login-menu");
+const register_menu = document.getElementById("register-menu");
 
-function openReportMenu(){
-  report_menu.style.display = "block"
+report_menu.style.display = "none";
+//login_menu.style.display = "none";
+register_menu.style.display = "none";
+function openMenu(menu){
+  menu.style.display = "block"
 };
 
-function closeReportMenu(){
-  report_menu.style.display = "none"
+function closeMenu(menu){
+  menu.style.display = "none"
 };
